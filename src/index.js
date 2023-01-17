e125c6a4d30a01d6da1db3d84bd12747c36f4395
Elkaouni <elkaouni.yousra@gmail.com>
2023-01-17 13:38:25 +0000

    login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Home from "./Home";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+        <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
